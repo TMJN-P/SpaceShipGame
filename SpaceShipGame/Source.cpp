7226d8c47b44a3d48ab4524daa0f09961fb2209d
TMJN <tomo.hoshii47@gmail.com>
2019-02-28 13:05:23 +0900

    何故かエラーを吐いています 原因はわかりません

--- SpaceShipGame/Source.cpp
+++ SpaceShipGame/Source.cpp
@@ -4,10 +4,11 @@
 const int FieldX = 4000, FieldY = 4000;
 const double TurnMove = 0.1, Acceleration = 0.12, Gravity = 0.5;
 char Buf[256];
-double VectorX, VectorY, Angle, BackX, BackY;
-int SpaceShipPicture, SpacePicture, MiniMap;
+double VectorX, VectorY, Angle, BackX, BackY, UseBrake;
+int SpaceShipPicture, SpacePicture, SpaceShipBrokenPicture;
 
 void DrawShip();
+void DrawBrokenShip();
 void Init();
 
 
@@ -42,22 +43,37 @@ public:
 		VectorY -= Gravity * (Y - 320)*pow(1.005, -max(Distance,300.0)) / Distance;
 	}
 	int Landing() {
-		DrawFormatString(X, Y, GetColor(255, 255, 255), "%f", atan2(Y-300, X-300));
-		if (Dist() <= Radius + 10) {
-			if ((fmod(abs(atan2(Y - 300, X - 300) - Angle), DX_PI * 2) <= 0.5 || abs(fmod(DX_PI * 2 - abs(atan2(Y - 300, X - 300) - Angle), DX_PI * 2)) <= 0.5)&&sqrt(VectorX*VectorX + VectorY * VectorY) <= 2) {
-				DrawFormatString(50, 50, GetColor(255, 150, 0), "Game Clear");
+		DrawFormatString(X, Y, GetColor(255, 255, 255), "%f", atan2(Y-320, X-320));
+		if (Dist() <= Radius + 14) {
+			if ((fmod(abs(atan2(Y - 320, X - 320) - Angle), DX_PI * 2) <= 0.5 || abs(fmod(DX_PI * 2 - abs(atan2(Y - 320, X - 320) - Angle), DX_PI * 2)) <= 0.5)&&sqrt(VectorX*VectorX + VectorY * VectorY) <= 2) {
+				DrawBoxAA(50, 50, 590, 310, GetColor(0, 0, 0), TRUE);
+				DrawBoxAA(50, 50, 590, 310, GetColor(255, 255, 255), FALSE);
+				SetFontSize(100);
+				DrawString(70, 70, "Game Clear", GetColor(255, 125, 0));
+				int Score = (2 - min(fmod(abs(atan2(Y - 320, X - 320) - Angle), DX_PI * 2), abs(fmod(DX_PI * 2 - abs(atan2(Y - 320, X - 320) - Angle), DX_PI * 2))))*(10000-Radius*20)*(5 - sqrt(VectorX*VectorX + VectorY * VectorY));
+				if (!UseBrake) {
+					Score *= 2;
+					SetFontSize(30);
+					DrawString(180, 180, "No Brake Bonus ~2", GetColor(255, 175, 0));
+				}
+				SetFontSize(70);
+				DrawFormatString(70, 220, GetColor(255,255,255),"Score:%d",Score);
+				DrawString(70, 400, "Press Enter to Return Title", GetColor(255, 255, 255));
 				DrawShip();
 				ScreenFlip();
-				WaitKey();
+				while (!Buf[KEY_INPUT_RETURN]) {
+					GetHitKeyStateAll(Buf);
+				}
 				Init();
-				return 1;
+				return 0;
 			}
 			else {
 				DrawFormatString(50, 50, GetColor(255, 255, 255), "Game Over");
+				DrawBrokenShip();
 				ScreenFlip();
 				WaitKey();
 				Init();
-				return -1;
+				return 0;
 			}
 		}
 		else return 0;
@@ -80,6 +96,11 @@ void Accelerate() {
 void DrawShip() {
 	DrawRotaGraph(320, 320, 0.5, Angle, SpaceShipPicture, TRUE);
 }
+
+void DrawBrokenShip() {
+	DrawRotaGraph(320, 320, 0.5, Angle, SpaceShipBrokenPicture, TRUE);
+}
+
 void DrawShipMiniMap() {
 	DrawRotaGraph(320, 320, 0.3, Angle, SpaceShipPicture, TRUE);
 }
@@ -114,19 +135,31 @@ void DrawMap() {
 	DrawShipMiniMap();
 }
 
+void Brake() {
+	UseBrake = TRUE;
+	VectorX *= 0.97;
+	VectorY *= 0.97;
+	for (int i = 0; i != 20; ++i) {
+		DrawCircleAA(320, 320, i + 10, 60, GetColor(0, 0, 255 - i * 10), FALSE);
+	}
+}
+
 
 
 void Init() {
+	SetFontSize(20);
 	VectorX = 0;
 	VectorY = 0;
 	SpacePicture = LoadGraph("‰F’ˆ.png");
 	SpaceShipPicture = LoadGraph("’…—¤‘D.png");
+	SpaceShipBrokenPicture = LoadGraph("”š”­.png");
+	UseBrake = FALSE;
 	Angle = DX_PI / 2;
 	StarData[0] = { -150,-900,100,180,255,40 };
 	StarData[1] = { -1000,-2100,150,0,190,120 };
 	StarData[2] = { -1500,-2100,200,255,20,10 };
 	StarData[3] = { -2100,-3000,150,10,255,100 };
-	StarData[4] = { -2000,-150,50,100,255,10 };
+	StarData[4] = { -2000,-150,25,100,255,10 };
 	StarData[5] = { 300,-2200,50,255,140,0 };
 	StarData[6] = { 2700,-2300,150,100,10,250 };
 	StarData[7] = { 1800,-1250,250,10,140,255 };
@@ -140,7 +173,7 @@ void Init() {
 	StarData[15] = { 450,2500,100,200,100,100 };
 	StarData[16] = { 900,1500,75,100,200,100 };
 	StarData[17] = { 2000,900,150,100,100,200 };
-	StarData[18] = { 3000,3600,150,100,100,250 };
+	StarData[18] = { 3000,3600,300,100,100,250 };
 	StarData[19] = { 2400,2700,200,200,200,200 };
 }
 
@@ -152,8 +185,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	SetGraphMode(640, 640, 32);
 	SetWindowSize(640, 640);
 	SetWindowText("’…—¤ƒQ[ƒ€");
-	SetFontSize(20);
-	//	SetUse3DFlag(false);
 	if (DxLib_Init() == -1)
 		return -1;
 	Init();
@@ -168,12 +199,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		if (Buf[KEY_INPUT_LEFT]) {
 			TurnLeft();
 		}
+		if (Buf[KEY_INPUT_DOWN]) {
+			Brake();
+		}
 		if (Buf[KEY_INPUT_UP]) {
 			Accelerate();
-		}
-		if (Buf[KEY_INPUT_DOWN]) {
-			VectorX *= 0.97;
-			VectorY *= 0.97;
 		}
 		BackMove();
 		for (int i = 0; i != 20; ++i) {
@@ -182,10 +212,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			StarData[i].ShipMove();
 			StarData[i].Landing();
 		}
+
+		DrawShip();
 		if (Buf[KEY_INPUT_M]) {
 			DrawMap();
 		}
-		DrawShip();
 		DrawFormatString(0, 0, GetColor(255, 255, 255), "%f %f %f", VectorX, VectorY, fmod(Angle, DX_PI * 2));
 		ScreenFlip();
 	}
